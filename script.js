ee07378f16be1f4121e716e698177c9e2a6078c4
JayanaH7 <126017621+JayanaH7@users.noreply.github.com>
2023-02-21 11:36:47 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,33 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+
+let goInTheHouse = document.querySelector(".option-one1");
+let letsGoUpstairsScreen = document.querySelector(".option-one-screen");
+goInTheHouse.onclick = function() {
+    letsGoUpstairsScreen.style.display = "block";
+};
+let GoInTheKitchen = document.querySelector(".option-one1");
+let GoInTheBedroom = document.querySelector(".option-one-screen");
+GoInTheKitchen.onclick = function() {
+ GoInTheBedroom .style.display = "block";
+};
+
+
+
+
+
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
